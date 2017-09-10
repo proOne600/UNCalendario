0db8a1959a4b01b4603169a3d40fef85a577dbab
Bryan Antonio Angarita Rodriguez <baangaritar@unal.edu.co>
2017-09-10 00:11:40 -0500

    Modificaciones al CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'home/contact'
   devise_for :users
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
