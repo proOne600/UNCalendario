586b4389e5d1e98e91ad67c7cf2bdcbcc6aab3d6
Bryan Antonio Angarita Rodriguez <baangaritar@unal.edu.co>
2017-09-09 23:41:12 -0500

    Cambio color de formularios

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'home/index'
-  get 'home/contact'
   devise_for :users
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
