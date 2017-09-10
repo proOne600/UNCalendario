73c6389e66d66d571a7d65a9d5ef94e68ca3d910
Bryan Antonio Angarita Rodriguez <baangaritar@unal.edu.co>
2017-09-09 23:34:08 -0500

    Agregadas vistas de Registro, Inicio de Sesión

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,10 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( main.css )
+Rails.application.config.assets.precompile += %w( jquery-3.2.1.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
