a7a732836a6c61241b65ccc5b822b6e6015ac448
David <dfricoh@unal.edu.co>
2017-10-04 20:50:40 +0000

    Avance clase 4 de octubre

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -83,7 +83,7 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      #params.require(:event).permit(:name, :description, :published, :cancelled, :id_user, :event_date, :event_init_hour, :event_end_hour, :even_end_date, :calification, :all_calification)
       params.require(:event).permit(:name, :description, :published, :cancelled, :id_user, :event_date, :event_init_hour, :event_end_hour, :even_end_date, :calification, :all_calification)
+      #params.require(:event).permit(:name, :description, :published, :cancelled, @users.id, :event_date, :event_init_hour, :event_end_hour, :even_end_date, :calification, :all_calification)
     end
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,5 @@
 class ContactMailer < ApplicationMailer
-<<<<<<< HEAD
-    default from: “UNCalendario <contacto@UNCalendario.co>”
-=======
     
->>>>>>> DavidRico
     
     def 
         contact_send(params) @parameters=params mail(to:@parameters[:email],subject:@parameters[:subject]) 
