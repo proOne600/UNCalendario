b1638cdca3b65f37d8c6a7086c4d552a35e052b1
David <dfricoh@unal.edu.co>
2017-10-04 20:41:08 +0000

    Mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ApplicationMailer
-    default from: “UNCalendario <contacto@UNCalendario.co>”
+    
     
     def 
-        contact_send(params) @parameters=params mail(to:’tuemail@example.com’,subject:@parameters[:subject]) 
+        contact_send(params) @parameters=params mail(to:@parameters[:email],subject:@parameters[:subject]) 
     end
 end
