19b772f4415a71fe46b74f7e8dee194c292b876c
David <dfricoh@unal.edu.co>
2017-10-04 20:31:17 +0000

    Agregar contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,3 +1,7 @@
 class ContactMailer < ApplicationMailer
     default from: “UNCalendario <contacto@UNCalendario.co>”
+    
+    def 
+        contact_send(params) @parameters=params mail(to:’tuemail@example.com’,subject:@parameters[:subject]) 
+    end
 end
