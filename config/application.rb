5b421f412c147a0266b312da42289d02252b2383
Miguel Tovar <matovaro@unal.edu.co>
2017-10-17 20:39:02 +0000

    Added Time zone Bogota

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module UNCalendario
     config.load_defaults 5.1
     config.assets.enabled = true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.time_zone = "Bogota"
+    config.active_record.default_timezone= :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
