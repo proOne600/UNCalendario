05dc140cdd937b6cf4997ac3bcbe5de0974398f5
Daniel Cifuentes Mac <danielguarnizo@gmail.com>
2017-10-25 22:50:20 -0500

    Updated fields events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -29,7 +29,6 @@ class EventsController < ApplicationController
   # GET /events/1
   # GET /events/1.json
   def show
-    @category = Category.find(@event.category_id)
   end
 
   # GET /events/new
