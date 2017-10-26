2824055ffc610f3191e3efbe2422f52d3bfe3225
Miguel Tovar <matovaro@unal.edu.co>
2017-10-26 07:08:56 +0000

    Dowload stadistics

--- app/controllers/stadistics_controller.rb
+++ app/controllers/stadistics_controller.rb
@@ -6,4 +6,22 @@ class StadisticsController < ApplicationController
 	    @women=User.total_women
 	    @men=User.total_men
 	end
+	
+	def get_data
+        send_data generate_stadistics,
+              filename: "Estadisticas.pdf",
+              type: "application/pdf"
+    end
+	
+	private
+	
+	def generate_stadistics
+        Prawn::Document.new do
+            text "Estadisticas", align: :center
+            text "Numero total de eventos: #{Event.total_size}"
+            text "Numero total de usuarios: #{User.total_size}"
+            text "Mujeres registradas: #{User.total_women}"
+            text "Hombres registrados: #{User.total_men}"
+        end.render
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,7 @@ Rails.application.routes.draw do
   root to: 'events#index'
   get '/contact' => 'home#contact'
   get '/stadistics' =>'stadistics#show'
+  get '/stadistics/generate' => 'stadistics#get_data'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #get ‘send’ => ‘send#index’
