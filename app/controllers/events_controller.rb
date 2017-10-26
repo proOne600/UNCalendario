cc6f0487e1170ae70935f54351957a4c4220469c
Miguel Tovar <matovaro@unal.edu.co>
2017-10-26 01:25:01 +0000

    Added category to PDF

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -134,6 +134,7 @@ class EventsController < ApplicationController
     Prawn::Document.new do
       text event.name, align: :center
       text "Descripcion: #{event.description}"
+      text "Categoria: #{Category.find(event.category_id).name}"
       if event.event_date.present?
         text "Fecha inicio: #{event.event_date.to_formatted_s(:short)}"
       end
