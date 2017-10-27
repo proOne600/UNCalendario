d2ee1267296a4ff95eb44f18b5bd09b855322d20
davidrh195 <dfricoh@unal.edu.co>
2017-10-26 19:31:05 -0500

    Social Network auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def all_prividers
+  def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
@@ -11,6 +11,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
-  alias_method :facebook,:google_oauth2,:all_prividers
+  def google_oauth2
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+    else
+      session['devise.google_data'] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
+    end
+  end
 
 end
