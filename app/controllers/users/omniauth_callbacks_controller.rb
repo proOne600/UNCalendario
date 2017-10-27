252820a44f24942f2a5c6bab16134b9ca63815a6
davidrh195 <dfricoh@unal.edu.co>
2017-10-26 20:49:30 -0500

    nada importante

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -16,10 +16,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+      #set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
     else
       session['devise.google_data'] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
+      #redirect_to new_user_registration_url
     end
   end
 
