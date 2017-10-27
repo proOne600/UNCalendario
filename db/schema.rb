f249cbcde7cb6bf5e5cb8cca3ee7dcf7e4ccb66a
Daniel Cifuentes Mac <danielguarnizo@gmail.com>
2017-10-26 21:42:11 -0500

    Fixed migration.

--- db/schema.rb
+++ db/schema.rb
@@ -158,11 +158,11 @@ ActiveRecord::Schema.define(version: 20171026192335) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.string "name"
     t.datetime "date_of_birth"
     t.boolean "is_female", default: false
-    t.string "provider"
-    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
