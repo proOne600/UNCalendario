7fbcd3dbd260fb8b44ceeb55e9586441000d29ee
Daniel Cifuentes Mac <danielguarnizo@gmail.com>
2017-10-27 01:18:01 -0500

    restored geocoding

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,7 +15,8 @@ class Event < ApplicationRecord
     # mount_uploader :image, ImageUploader
     validates :name, :description, presence: true
 
-    
+    geocoded_by :address
+after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
 
 
     # attr_accessible :id_user
