75728a692c12e11c187fb4860801c59d4e116fd2
Unknown <baangaritar@unal.edu.co>
2017-11-07 16:13:22 -0500

    Agregar estilos a registro y edición de perfil
    :D

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   protected
  
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me, :avatar, :avatar_cache) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :password, :password_confirmation, :current_password, :avatar, :avatar_cache, :remove_avatar) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :date_of_birth, :is_female, :remember_me, :avatar, :avatar_cache) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :password, :password_confirmation, :date_of_birth, :is_female, :current_password, :avatar, :avatar_cache, :remove_avatar) }
   end
 #	@events.
 
