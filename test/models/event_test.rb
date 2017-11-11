d66c3d7f0dca72e1dae6bd73028f3bdc7aed3bd0
Daniel Cifuentes Mac <danielguarnizo@gmail.com>
2017-11-11 02:27:04 -0500

    base test

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -2,8 +2,24 @@ require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
   fixtures :events
-  test "get events" do
+  test 'should get event' do
     event_example = events(:one)
     assert_equal(Event.find(event_example.id), event_example)
+  end
+
+  test 'should not save event without required fields' do
+    event = Event.new
+    assert_not event.save
+  end
+
+  test 'should get no events' do
+    events_col = Event.search('asdf', '')
+    assert_not_includes(events_col, events(:one))
+  end
+
+  test 'should include events in results' do
+    events_col = Event.search(nil,nil).pluck(:id)
+    event_example = events(:two)
+    assert(events_col.include?(event_example.id), event_example.id)
   end
 end
