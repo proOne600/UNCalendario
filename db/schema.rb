2308b97c89d8b3efe6345c48f084aff9cc992424
Daniel Cifuentes Mac <danielguarnizo@gmail.com>
2017-11-22 01:47:57 -0500

    Fixed avatar missing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107130941) do
+ActiveRecord::Schema.define(version: 20171122063920) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer "user_id"
